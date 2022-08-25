e569fd477abe317043b99db316281c72d1424ccb
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-25 16:33:46 +0300

    COMPLETED: many to many associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+    # Active Record won't know how to go through the reviews table
+    # until you create the has_many :reviews association => order matters!
+    has_many :reviews
+    has_many :games, through: :reviews
 end
