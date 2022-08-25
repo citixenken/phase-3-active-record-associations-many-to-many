79c0e2efedbc5e19a256f759e8c0a229f0296a61
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-25 16:32:37 +0300

    commit tree 81bca9af000c8ffe55da82c9684094170968aa9b

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
-  has_many :reviews
+  # when we use the has_many macro, Active Record generates an instance method 
+  # #reviews that we can call on a Game instance to access all the associated reviews
+  has_many :reviews # 1-* relationship
+  has_many :users, through: :reviews # *-* relationship
 end
